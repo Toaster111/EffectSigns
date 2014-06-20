183a21048d6f1cc9bc9a72957d612a5b2ccd6748
Toaster111 <sylvamj@comcast.net>
2014-06-20 17:03:46 -0400

    Update to Main class
    new implements to SignsHandler class and creating config and datafolder

--- src/EffectSigns/EffectSigns.java
+++ src/EffectSigns/EffectSigns.java
@@ -6,10 +6,10 @@ import org.bukkit.plugin.PluginDescriptionFile;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public class EffectSigns extends JavaPlugin{
-	
+
 	public final Logger logger = Logger.getLogger("Minecraft");
 	public static EffectSigns plugin;
-	
+
 	@Override
 	public void onDisable()
 	{
@@ -20,13 +20,18 @@ public class EffectSigns extends JavaPlugin{
 	@Override
 	public void onEnable()
 	{
+		plugin.getDataFolder().mkdir();
+		final FileConfiguration config = this.getConfig();
 		PluginDescriptionFile pdfFile = this.getDescription();
 		this.logger.info(pdfFile.getName() +  " Has Been Enabled!");
 		
+		//not sure if that will work check it for me vvv
+		getServer().getPluginManager().registerEvent(SignsHandler(), this);
+		
 		getCommand("es").setExecutor(new CommandHandler());
 	}
-	
-	
-	
-	
+
+
+
+
 }
